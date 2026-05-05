d6431e319e8bff5255a2eaf6b75b55366b9040d9
sammo <sammo@Sammuel>
2026-05-05 09:56:08 -0500

    codigo corregido correspondiendo a los errores

--- src/Subordinates.java
+++ src/Subordinates.java
@@ -2,24 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Subordinates {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        int[] sub = new int[n + 1];
+        
         int[] boss = new int[n + 1];
+        int[] hijosPendientes = new int[n + 1];
+        int[] sub = new int[n + 1];
 
         if (n > 1) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 2; i <= n; i++) {
-                boss[i] = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                boss[i] = b;
+                hijosPendientes[b]++;
             }
         }
 
-        for (int i = n; i >= 2; i--) {
-            int miJefe = boss[i];
-            sub[miJefe] += sub[i] + 1;
+        Deque<Integer> cola = new ArrayDeque<>();
+        for (int i = 1; i <= n; i++) {
+            if (hijosPendientes[i] == 0) {
+                cola.add(i);
+            }
+        }
+
+        while (!cola.isEmpty()) {
+            int actual = cola.poll();
+            if (actual == 1) continue;
+
+            int miJefe = boss[actual];
+            sub[miJefe] += sub[actual] + 1;
+            hijosPendientes[miJefe]--;
+
+            if (hijosPendientes[miJefe] == 0) {
+                cola.add(miJefe);
+            }
         }
 
         StringBuilder sb = new StringBuilder();
