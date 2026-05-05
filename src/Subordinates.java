cefdebbe7244c987e0d983112434bbf5563dca61
sammo <sammo@Sammuel>
2026-05-05 09:49:09 -0500

    se vuelve a corregir el codigo segun los errores de cses

--- src/Subordinates.java
+++ src/Subordinates.java
@@ -3,42 +3,29 @@ import java.util.*;
 
 public class Subordinates {
 
-    static List<Integer>[] tree;
-    static int[] sub;
-
-    static void dfs(int node) {
-        for (int child : tree[node]) {
-            dfs(child);
-
-            sub[node] += sub[child] + 1;
-        }
-    }
-
     public static void main(String[] args) throws Exception {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        tree = new ArrayList[n + 1];
-        sub = new int[n + 1];
+        int[] sub = new int[n + 1];
+        int[] boss = new int[n + 1];
 
-        for (int i = 1; i <= n; i++) {
-            tree[i] = new ArrayList<>();
+        if (n > 1) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 2; i <= n; i++) {
+                boss[i] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        String[] input = br.readLine().split(" ");
-        for (int i = 2; i <= n; i++) {
-            int boss = Integer.parseInt(input[i - 2]);
-            tree[boss].add(i);
+        for (int i = n; i >= 2; i--) {
+            int miJefe = boss[i];
+            sub[miJefe] += sub[i] + 1;
         }
-
-        dfs(1);
 
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
             sb.append(sub[i]).append(" ");
         }
-
         System.out.println(sb);
     }
 }
